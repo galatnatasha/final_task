417e688ab07b3f4b41ce08fc582b65b61b42ed2d
natalia_galat <n9885736577@yandex.ru>
2022-07-27 12:43:52 +0300

    code done

--- code/Program.cs
+++ code/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+
+string [] array = {"hellow", "2", "world", ":-)"};
+string [] array2 = new string [array.Length];
+int index2 = 0;
+
+for (int index = 0; index < array.Length; index++)
+{
+    if (array[index].Length <= 3)
+    {
+        array2[index2] = array[index];
+        index2++;
+    }
+}
+PrintArray(array2);
